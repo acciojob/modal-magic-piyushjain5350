d4502bb2a1c0a6ae71448ea2e797bccbe47863f4
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2023-12-21 23:08:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const modal = document.getElementById('modal')
+const open = document.getElementById('openModal')
+function handleClick(){
+	modal.style.display = "block"
+}
+function handleClose(){
+	modal.style.display = "none"
+}
+// window.addEventListener("click", function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// });
+
+window.addEventListener('click',(e)=>{
+	if(e.target !== open && !modal.contains(e.target)){
+	handleClose()
+	}
+})
