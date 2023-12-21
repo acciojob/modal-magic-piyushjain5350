f830dee96753ea3a485246281c7fbce689ef686f
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2023-12-21 23:14:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 //your JS code here. If required.
-const modal = document.getElementById('modal')
-const open = document.getElementById('openModal')
-function handleClick(){
-	modal.style.display = "block"
-}
-function handleClose(){
-	modal.style.display = "none"
-}
-// window.addEventListener("click", function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// });
+// const modal = document.getElementById('modal')
+// const open = document.getElementById('openModal')
+// function handleClick(){
+// 	modal.style.display = "block"
+// }
+// function handleClose(){
+// 	modal.style.display = "none"
+// }
+// // window.addEventListener("click", function(event) {
+// //   if (event.target == modal) {
+// //     modal.style.display = "none";
+// //   }
+// // });
 
-window.addEventListener('click',(e)=>{
-	if(e.target !== open && !modal.contains(e.target)){
-	handleClose()
-	}
-})
+// window.addEventListener('click',(e)=>{
+// 	if(e.target !== open && !modal.contains(e.target)){
+// 	handleClose()
+// 	}
+// })
+//your JS code here. If required.
+document.getElementById("openModalBtn").addEventListener("click", function() {
+  document.getElementById("modal").style.display = "block";
+});
+
+document.getElementById("closeModalBtn").addEventListener("click", function() {
+  document.getElementById("modal").style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target == document.getElementById("modal")) {
+    document.getElementById("modal").style.display = "none";
+  }
+});
